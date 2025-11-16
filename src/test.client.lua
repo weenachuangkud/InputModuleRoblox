6831ca021f6974529af8d49dcfd782620d28fca7
Mawin <weena.chuangkud@gmail.com>
2025-11-16 19:43:47 +0700

    Update test.client.lua

--- src/test.client.lua
+++ src/test.client.lua
@@ -5,3 +5,21 @@ local InputService = require(PathtoModule:WaitForChild("InputService"))
 local InputConfigs = require(PathToModule:WaitForChild("InputConfigs"))
 
 -- Test
+InputConfigs.Click.OnInputBegan = function()
+  print("Player OnClick Start")
+end
+
+InputConfigs.Click.OnInputEnded = function()
+  print("Player OnClick Ended")
+end
+
+InputConfigs.PressR.OnInputBegan = function()
+  print("Player OnPressR Start")
+end
+
+InputConfigs.PressR.OnInputEnded = function()
+  print("Player OnPressR Ended")
+end
+
+-- recommended
+InputService.Bind(
