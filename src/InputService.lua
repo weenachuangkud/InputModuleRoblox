f99dd81e3f94e2b84aee0dd158f421af94b7537e
Mawin Chuangkud <weena.chuangkud@gmail.com>
2025-12-14 03:00:38 +0700

    Update InputService.lua

--- src/InputService.lua
+++ src/InputService.lua
@@ -37,6 +37,7 @@ function InputService.Bind(
 	name: string,
 	cfg: InputConfigs.InputConfig
 )
+	if not cfg then warn("InputConfig is nil") return end
 	-- Mobile GUI button (optional)
 	if cfg.MobileButton and InputConfigs.IsMobile then
 		local button : ImageButton | TextButton = cfg.MobileButton
